6497feaf5d7cb951b9256b0d790bf1469d42f1da
Boualem234 <2534056@etu.cchic.ca>
2025-12-02 17:02:40 -0500

    Initial commit

---
+++ GestionnaireLicences/GestionnaireLicences/GestionnaireLicences/DataAccessLayer/Factories/Base/FactoryBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GestionnaireLicences.SQL;
+using Microsoft.Extensions.Configuration;
+
+namespace GestionnaireLicences.DataAccessLayer.Factories.Base
+{
+    public class FactoryBase
+    {
+        private string _cnnStr = string.Empty;
+
+        public string CnnStr
+        {
+            get
+            {
+                if (_cnnStr == string.Empty)
+                {
+                    try
+                    {
+                        var basePath = AppDomain.CurrentDomain.BaseDirectory;
+                        var config = new ConfigurationBuilder()
+                            .SetBasePath(basePath)
+                            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                            .Build();
+                        
+                        var sectionConnectionString = config.GetSection("ConnectionString");
+                        ApiRestConnectionString connectionString = sectionConnectionString.Get<ApiRestConnectionString>();
+                        _cnnStr = connectionString.BuildConnectionString();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception($"Erreur lors de la lecture de appsettings.json : {ex.Message}", ex);
+                    }
+                }
+
+                return _cnnStr;
+            }
+        }
+    }
+}
